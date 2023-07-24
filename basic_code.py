a1e751e9d9b321cfeac14f5a0b7642c503f03b35
JulieSoboleva <ju.soboll@gmail.com>
2023-07-24 16:58:58 +0300

    Add vk_bot file

--- basic_code.py
+++ basic_code.py
@@ -1,8 +1,9 @@
+from vk_bot import VkBot
 from random import randrange
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 
-token = input('Token: ')
+token = 'vk1.a.tjL8Ri4s2zRcPz3aB6CiZXZq2Undzl3sHeLV3tH6GGxQbPMYEJmfHIqNDoDaUw3e3MHnXjj3oI8Tx8KPQEepj5Zvh-P4rT2UUSbtirOtejoP1_2qCjGlKgqUOZFblASs4F4XjGMxtO7v2E8tvLXXNqoNABKmaBDoemiwniObrpbe3devZqxu9_QPidhPJjBnx0u6TB35NsQ6COelZmm4Aw'  # input('Token: ')
 
 vk = vk_api.VkApi(token=token)
 longpoll = VkLongPoll(vk)
@@ -13,15 +14,16 @@ def write_msg(user_id, message):
                                 'random_id': randrange(10 ** 7), })
 
 
+print("Server started")
 for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW:
 
         if event.to_me:
             request = event.text
 
-            if request == "привет":
-                write_msg(event.user_id, f"Хай, {event.user_id}")
-            elif request == "пока":
-                write_msg(event.user_id, "Пока((")
-            else:
-                write_msg(event.user_id, "Не поняла вашего ответа...")
+            print(f'New message for me from user with ID: {event.user_id}', end='')
+
+            bot = VkBot(event.user_id)
+            write_msg(event.user_id, bot.new_message(event.text))
+
+            print('Text: ', event.text)
